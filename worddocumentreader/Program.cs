65a3443ce100bf7b6505de71fcd2101a34c41e71
Meghaa Maheshwari <meghaharlalka@gmail.com>
2015-05-09 22:32:47 +0800

    First commit

---
+++ worddocumentreader/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Novacode;
+using System.IO;
+
+
+namespace worddocumentreader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+           // System.Diagnostics.Debugger.Launch();
+
+            Dictionary<string, float> TextVsValue = new Dictionary<string, float>();
+
+            using (DocX doc = DocX.Load("d:\\Sample.docx"))
+            {              
+                
+                for (int i = 0; i < doc.Paragraphs.Count; i++)
+                {
+                                        
+                    foreach (var item in doc.Paragraphs[i].Text.Split(new string[] { "\n" }
+                              , StringSplitOptions.RemoveEmptyEntries))
+                    {
+                        foreach (string searchText in args)
+                        {
+                            if (item.ToLower().Contains(searchText.ToLower()))
+                            {
+                                // Highlight text using an indirect method of replace since docX currently
+                                //does not have support to highlight just a single word.
+                                if (doc.Paragraphs[i] is Paragraph)
+                                {
+                                    Paragraph sen = doc.Paragraphs[i] as Paragraph;
+                                    Formatting form = new Formatting();
+                                    form.Highlight = Highlight.yellow;
+                                    form.Bold = true;
+                                    sen.ReplaceText(searchText, searchText, false, System.Text.RegularExpressions.RegexOptions.IgnoreCase,
+                                        form, null, MatchFormattingOptions.ExactMatch);
+                                }
+                                StringAndValue SearchString = new StringAndValue(item, searchText);
+                                float val = SearchString.ExtractStringAndValue();
+                                if (!TextVsValue.ContainsKey(searchText))
+                                {
+                                    TextVsValue.Add(searchText, val);
+                                }
+                                else if(TextVsValue[searchText] < val)
+                                {
+                                    TextVsValue[searchText] = val;
+                                }
+                                doc.Save();
+                            }
+                        }
+                    }
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ worddocumentreader/StringAndValue.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace worddocumentreader
+{
+    class StringAndValue
+    {
+
+        private string sentence_in_word_doc;
+
+        private string string_to_search;
+
+        private string [] _currencySymbols = new string [] { "$","zł", "€", "£" };
+
+        public string SentenceInWordDoc
+        {
+            get
+            {
+                return sentence_in_word_doc;
+            }
+            set
+            {
+                sentence_in_word_doc = value;
+            }
+        }
+
+        public string StringToSearch
+        {
+            get
+            {
+                return string_to_search;
+            }
+        }
+
+        public StringAndValue(string sentence, string tosearch)
+        {
+            this.sentence_in_word_doc = sentence;
+            this.string_to_search = tosearch;
+        }
+
+        public float ExtractStringAndValue()
+        {
+            float Value = 0;
+            char [] delimiters = new [] { ',', ';', ' ','.' };
+            if (_currencySymbols.Where(x => SentenceInWordDoc.Contains(x)).Count() > 0)
+            {
+                foreach (string curr in _currencySymbols)
+                {
+                    if (SentenceInWordDoc.Contains(curr))
+                    {
+                        string NewString = SentenceInWordDoc.Replace(curr, "");
+                        SentenceInWordDoc = NewString;
+                    }
+                }
+            }
+      
+            string [] SplittedArray = SentenceInWordDoc.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < SplittedArray.Length; i++)
+            {
+                if (SplittedArray[i].ToLower().Equals(string_to_search))
+                {
+                    Value = GetNumber(SplittedArray,SplittedArray.Length,i,1);
+                    break;
+                }
+            }
+            return Value;
+        }
+
+
+        //The value can be either anywhere before or after the string. Hence look for the first number
+        // before or after the string and stop
+        private float GetNumber(string [] Array, int length, int CurrentIndex, int i)
+        {
+            float n;
+            float ReturnValue = 0;
+            bool ValFound = false;
+
+            if(((CurrentIndex - i) >= 0) && (float.TryParse(Array[CurrentIndex-i], out n)))          
+            {
+                  ReturnValue = float.Parse(Array[CurrentIndex-i]);
+                  ValFound = true;
+            }
+            else if(((CurrentIndex + i) < length) && (float.TryParse(Array[CurrentIndex + i], out n)))
+            {
+                ReturnValue = float.Parse(Array[CurrentIndex + i]);
+                ValFound = true;               
+            }
+            if (!ValFound && (i < length))
+            {
+                GetNumber(Array, length, CurrentIndex, i+1);
+            }
+
+            return ReturnValue;
+        }
+
+    }
+}
