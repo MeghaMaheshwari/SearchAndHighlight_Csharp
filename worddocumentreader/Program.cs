82280715d5242e17f82ad254a70f9ff222fb56b2
Megha Maheshwari <meghaharlalka@gmail.com>
2015-06-04 14:02:03 +0800

    Changed the code to read the input from commandline

--- worddocumentreader/Program.cs
+++ worddocumentreader/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using Novacode;
 using System.IO;
+using System.Reflection;
 
 
 namespace worddocumentreader
@@ -15,49 +16,92 @@ namespace worddocumentreader
 
            // System.Diagnostics.Debugger.Launch();
 
+            if (args.Length < 1)
+            {
+                Print_Help();
+                Console.ReadKey();
+
+            }           
             Dictionary<string, float> TextVsValue = new Dictionary<string, float>();
 
-            using (DocX doc = DocX.Load("d:\\Sample.docx"))
-            {              
-                
-                for (int i = 0; i < doc.Paragraphs.Count; i++)
+            string input = args[0];
+
+            if (System.IO.File.Exists(input))
+            {
+                try
                 {
-                                        
-                    foreach (var item in doc.Paragraphs[i].Text.Split(new string[] { "\n" }
-                              , StringSplitOptions.RemoveEmptyEntries))
+                    using (DocX doc = DocX.Load(input))
                     {
-                        foreach (string searchText in args)
+                        for (int i = 0; i < doc.Paragraphs.Count; i++)
                         {
-                            if (item.ToLower().Contains(searchText.ToLower()))
+
+                            foreach (var item in doc.Paragraphs[i].Text.Split(new string[] { "\n" }
+                                      , StringSplitOptions.RemoveEmptyEntries))
                             {
-                                // Highlight text using an indirect method of replace since docX currently
-                                //does not have support to highlight just a single word.
-                                if (doc.Paragraphs[i] is Paragraph)
+                                foreach (string searchText in args)
                                 {
-                                    Paragraph sen = doc.Paragraphs[i] as Paragraph;
-                                    Formatting form = new Formatting();
-                                    form.Highlight = Highlight.yellow;
-                                    form.Bold = true;
-                                    sen.ReplaceText(searchText, searchText, false, System.Text.RegularExpressions.RegexOptions.IgnoreCase,
-                                        form, null, MatchFormattingOptions.ExactMatch);
+
+                                    if ((item.ToLower().Contains(searchText.ToLower())) && (searchText != input))
+                                    {
+                                        // Highlight text using an indirect method of replace since docX currently
+                                        //does not have support to highlight just a single word.
+                                        if (doc.Paragraphs[i] is Paragraph)
+                                        {
+                                            Paragraph sen = doc.Paragraphs[i] as Paragraph;
+                                            Formatting form = new Formatting();
+                                            form.Highlight = Highlight.yellow;
+                                            form.Bold = true;
+                                            sen.ReplaceText(searchText, searchText, false, System.Text.RegularExpressions.RegexOptions.IgnoreCase,
+                                                form, null, MatchFormattingOptions.ExactMatch);
+                                        }
+                                        StringAndValue SearchString = new StringAndValue(item, searchText);
+                                        float val = SearchString.ExtractStringAndValue();
+                                        if (!TextVsValue.ContainsKey(searchText))
+                                        {
+                                            TextVsValue.Add(searchText, val);
+                                        }
+                                        else if (TextVsValue[searchText] < val)
+                                        {
+                                            TextVsValue[searchText] = val;
+                                        }
+                                        doc.Save();
+                                    }
                                 }
-                                StringAndValue SearchString = new StringAndValue(item, searchText);
-                                float val = SearchString.ExtractStringAndValue();
-                                if (!TextVsValue.ContainsKey(searchText))
-                                {
-                                    TextVsValue.Add(searchText, val);
-                                }
-                                else if(TextVsValue[searchText] < val)
-                                {
-                                    TextVsValue[searchText] = val;
-                                }
-                                doc.Save();
                             }
                         }
+                        Console.ReadKey();
                     }
                 }
-                Console.ReadKey();
+                catch(SystemException ex)
+                {
+                    Console.WriteLine(ex);
+                }
+            }
+            else
+            {
+                Console.WriteLine("The file " + input + " does not exist");
             }
         }
+
+        private static void Print_Help()
+        {
+            Assembly myAssembly = Assembly.GetExecutingAssembly();
+
+            string description = "";
+            object[] attributes = myAssembly.GetCustomAttributes(typeof(AssemblyDescriptionAttribute), false);
+            if (attributes.Length > 0)
+            {
+                description = (attributes[0] as AssemblyDescriptionAttribute).Description;
+            }
+            Console.WriteLine();
+            Console.WriteLine("Mandatory command line arguments:");
+            Console.WriteLine("");
+            Console.WriteLine("The code accepts command line arguments");
+            Console.WriteLine("\t 1st argument : Specifies the path to the input file");
+            Console.WriteLine("\t 2nd argument : the words to be searched separated by spaces");
+            Console.WriteLine("Examples");
+            Console.WriteLine("  worddocumentreader.exe D://Sample.docx oranges apples");
+        }
+
     }
 }
